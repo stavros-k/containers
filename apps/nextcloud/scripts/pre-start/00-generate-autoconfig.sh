9109b7f930e55a93dac847ec5febcd74168be4eb
Stavros kois <s.kois@outlook.com>
2023-03-29 21:46:57 +0300

    PR

--- apps/nextcloud/scripts/pre-start/00-generate-autoconfig.sh
+++ apps/nextcloud/scripts/pre-start/00-generate-autoconfig.sh
@@ -79,6 +79,8 @@ create_config () {
 is_installed () {
   local conf_file='/var/www/html/config/config.php'
 
+  value=$(php -r "require('$conf_file'); echo \$CONFIG['installed'];")
+  echo "Value from PHP: $value"
   if [ -f "$conf_file" ]; then
     if grep -q -E 'installed.*=>.*true' "$conf_file"; then
       return 0
