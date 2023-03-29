b377dd72f0e94abef02f4d7c5d5da42c02530985
Stavros kois <s.kois@outlook.com>
2023-03-30 00:17:15 +0300

    comments

--- apps/nextcloud/scripts/pre-start/00-generate-autoconfig.sh
+++ apps/nextcloud/scripts/pre-start/00-generate-autoconfig.sh
@@ -87,7 +87,9 @@ create_autoconfig () {
 
 create_config_from_vars () {
   env | grep NEXTCLOUD_CONFIG_FILE_ | while read -r line; do
+    # Get the variable name (part before =)
     variable_name=$(echo "$line" | cut -d '=' -f 1 )
+    # Get the file name (part after NEXTCLOUD_CONFIG_FILE_)
     file_name=$(echo "$variable_name" | sed 's/NEXTCLOUD_CONFIG_FILE_//g' | tr '[:upper:]' '[:lower:]')
 
     echo "INFO: Printing contents of [$variable_name] to [$file_name]..."
@@ -98,6 +100,7 @@ create_config_from_vars () {
       echo "INFO: File [$conf_path] already exists, creating backup..."
       cp "$conf_path" "$conf_path.bak"
     fi
+    # Expand the "$variable_name" variable to get the value of that variable
     eval "echo \"\$$variable_name\"" > "$conf_path"
   done
 }
