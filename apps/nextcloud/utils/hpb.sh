e086c4fe0e7653ba856d11834f65c0cf3b082ab3
Stavros kois <s.kois@outlook.com>
2023-03-31 01:04:45 +0300

    lint

--- apps/nextcloud/utils/hpb.sh
+++ apps/nextcloud/utils/hpb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -n "$METRICS_PORT" ]; then
+if [ -z "$METRICS_PORT" ]; then
   echo 'NOTICE: METRICS_PORT not set, skipping metrics'
 fi
 
 /var/www/html/custom_apps/notify_push/bin/x86_64/notify_push \
-  --port ${NOTIFY_PUSH_PORT:-7867} \
+  --port "${NOTIFY_PUSH_PORT:-7867}" \
   --glob-config
   /var/www/html/config/config.php
 
