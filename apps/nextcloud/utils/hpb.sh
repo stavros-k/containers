946d21a8e2d3378db9232487e65407f1ddc0c721
Stavros kois <s.kois@outlook.com>
2023-03-31 01:04:01 +0300

    comment

--- apps/nextcloud/utils/hpb.sh
+++ apps/nextcloud/utils/hpb.sh
@@ -6,4 +6,8 @@ fi
 
 /var/www/html/custom_apps/notify_push/bin/x86_64/notify_push \
   --port ${NOTIFY_PUSH_PORT:-7867} \
+  --glob-config
   /var/www/html/config/config.php
+
+# Maybe build a custom image?
+# https://github.com/nextcloud/docker/pull/1466/files
