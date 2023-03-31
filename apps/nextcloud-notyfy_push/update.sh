ac0d0b127d52a25cd973d9d317d45668fe459b9f
Stavros kois <s.kois@outlook.com>
2023-03-31 22:09:38 +0300

    build notify

---
+++ apps/nextcloud-notyfy_push/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+notify_push_version="$(
+	git ls-remote --tags https://github.com/nextcloud/notify_push.git \
+		| cut -d/ -f3 \
+		| grep -vE -- '-rc|-b' \
+		| tr -d '^{}' \
+		| sed -E 's/^v//' \
+		| sort -V \
+		| tail -1
+)"
+
+echo "updating notify_push $notify_push_version"
+
+curr_dir=$(pwd)
+sed -re 's/^ENV NOTIFY_PUSH_VERSION .*$/ENV NOTIFY_PUSH_VERSION '"$notify_push_version"'/;' -i "$curr_dir/Dockerfile"
