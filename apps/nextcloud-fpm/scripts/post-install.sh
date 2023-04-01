9fa7d7e9841934f3da8f3f10bed8171cebe9b34e
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-01 22:49:07 +0300

    test occ

--- apps/nextcloud-fpm/scripts/post-install.sh
+++ apps/nextcloud-fpm/scripts/post-install.sh
@@ -1,4 +1,4 @@
-echo 'Applying PHP-FPM Tuning'
+echo 'Applying PHP-FPM Tuning...'
 echo 'You can change default values with the following variables'
 echo "PHP_MAX_CHILDREN      (Default: 20) Current: ${PHP_MAX_CHILDREN:-20}"
 echo "PHP_START_SERVERS     (Default: 5)  Current: ${PHP_START_SERVERS:-5}"
@@ -16,5 +16,18 @@ tune_file="/usr/local/etc/php-fpm/99-tune.conf"
   echo "pm.min_spare_servers = ${PHP_MIN_SPARE_SERVERS:-5}"
   echo "pm.max_spare_servers = ${PHP_MAX_SPARE_SERVERS:-15}"
 } > "$tune_file"
+
+echo "Tune file ($tune_file):"
+echo ''
+
+cat $tune_file
+
+echo '--------------------'
+
+occ config:system:set default_phone_region --value=GR
+
+echo '--------------------'
+
+echo 'Starting Nextcloud PHP-FPM'
 
 exec "$@"
