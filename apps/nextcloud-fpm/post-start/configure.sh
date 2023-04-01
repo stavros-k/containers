32ef4e57589d83d68fc57b10db88006011f5a4b2
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-02 00:59:47 +0300

    echo tings

--- apps/nextcloud-fpm/post-start/configure.sh
+++ apps/nextcloud-fpm/post-start/configure.sh
@@ -2,7 +2,11 @@
 
 echo 'Disabling WebUI Updater...'
 occ config:system:set upgrade.disable-web --type=bool --value=true
+
+echo 'Setting Default Phone Region...'
 occ config:system:set default_phone_region --value=${NEXT_DEFAULT_PHONE_REGION:-GR}
+
+echo 'Setting Overwrite URLs...'
 occ config:system:set overwrite.cli.url --value=${NEXT_OVERWRITE_CLI_URL:?"NEXT_OVERWRITE_CLI_URL is unset"}
 occ config:system:set overwritehost --value=${NEXT_OVERWRITE_HOST:?"NEXT_OVERWRITE_HOST is unset"}
 occ config:system:set overwriteprotocol --value=${NEXT_OVERWRITE_PROTOCOL:?"NEXT_OVERWRITE_PROTOCOL is unset"}
