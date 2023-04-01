c6c44eb18bc8ec20e5c204923b740f1e53a7f829
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-01 22:24:02 +0300

    print ...

--- apps/nextcloud-fpm/scripts/tune-php-fpm.sh
+++ apps/nextcloud-fpm/scripts/tune-php-fpm.sh
@@ -9,10 +9,12 @@ echo 'Visit https://spot13.com/pmcalculator to see what values you should set'
 
 tune_file="/usr/local/etc/php-fpm/99-tune.conf"
 
-echo '[www]'
-echo "pm.max_children = ${PHP_MAX_CHILDREN:-20}"
-echo "pm.start_servers = ${PHP_START_SERVERS:-5}"
-echo "pm.min_spare_servers = ${PHP_MIN_SPARE_SERVERS:-5}"
-echo "pm.max_spare_servers = ${PHP_MAX_SPARE_SERVERS:-15}"
+{
+  echo '[www]'
+  echo "pm.max_children = ${PHP_MAX_CHILDREN:-20}"
+  echo "pm.start_servers = ${PHP_START_SERVERS:-5}"
+  echo "pm.min_spare_servers = ${PHP_MIN_SPARE_SERVERS:-5}"
+  echo "pm.max_spare_servers = ${PHP_MAX_SPARE_SERVERS:-15}"
+} > "$tune_file"
 
 exec "$@"
