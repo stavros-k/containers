4dffe4a83055b9bf7b275522a9abaaacdcb1ea75
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-02 00:59:05 +0300

    add more

--- apps/nextcloud-fpm/post-start/configure.sh
+++ apps/nextcloud-fpm/post-start/configure.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo 'Disabling WebUI Updater...'
+occ config:system:set upgrade.disable-web --type=bool --value=true
 occ config:system:set default_phone_region --value=${NEXT_DEFAULT_PHONE_REGION:-GR}
 occ config:system:set overwrite.cli.url --value=${NEXT_OVERWRITE_CLI_URL:?"NEXT_OVERWRITE_CLI_URL is unset"}
 occ config:system:set overwritehost --value=${NEXT_OVERWRITE_HOST:?"NEXT_OVERWRITE_HOST is unset"}
 occ config:system:set overwriteprotocol --value=${NEXT_OVERWRITE_PROTOCOL:?"NEXT_OVERWRITE_PROTOCOL is unset"}
+
+echo 'Applying migrations/repairs/optimizations...'
+occ maintenance:repair
+occ db:add-missing-indices
+occ db:add-missing-columns
+occ db:add-missing-primary-keys
+yes | occ db:convert-filecache-bigint
+occ maintenance:mimetype:update-js
+occ maintenance:mimetype:update-db
