0b3e0326a7c820dd6312e124e67ae4abb5f98994
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-02 00:23:35 +0300

    config more

--- apps/nextcloud-fpm/post-start/configure.sh
+++ apps/nextcloud-fpm/post-start/configure.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 occ config:system:set default_phone_region --value=${NEXT_DEFAULT_PHONE_REGION:-GR}
+occ config:system:set overwrite.cli.url --value=${NEXT_OVERWRITE_CLI_URL:?"NEXT_OVERWRITE_CLI_URL is unset"}
+occ config:system:set overwritehost --value=${NEXT_OVERWRITE_HOST:?"NEXT_OVERWRITE_HOST is unset"}
+occ config:system:set overwriteprotocol --value=${NEXT_OVERWRITE_PROTOCOL:?"NEXT_OVERWRITE_PROTOCOL is unset"}
