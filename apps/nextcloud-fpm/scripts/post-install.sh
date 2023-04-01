67833cc365162d9ac37f484b63b12dc72dc5149a
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-01 23:41:19 +0300

    echo

--- apps/nextcloud-fpm/scripts/post-install.sh
+++ apps/nextcloud-fpm/scripts/post-install.sh
@@ -11,6 +11,7 @@ echo 'Visit https://spot13.com/pmcalculator to see what values you should set'
 tune_file="/usr/local/etc/php-fpm.d/99-tune.conf"
 
 if [ ! -d $(dirname $tune_file) ]; then
+  echo "creating..."
   mkdir -p $(dirname $tune_file)
 fi
 
