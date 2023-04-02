b5e7d891a09426c028937add6c13abd83c27eb1a
Stavros kois <s.kois@outlook.com>
2023-04-02 18:02:23 +0300

    set to file

--- apps/nextcloud-fpm/configure-scripts/occ-logging.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-logging.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 occ_logging(){
   echo '## Configuring Logging...'
+  occ config:system:set log_type --value="file"
+  occ config:system:set log_type_audit --value="file"
   occ config:system:set loglevel --value="${NX_LOG_LEVEL:-2}"
   occ config:system:set logfile --value="${NX_LOG_FILE:-"/var/www/html/data/nextcloud.log"}"
   occ config:system:set logfile_audit --value="${NX_LOG_FILE_AUDIT:-"/var/www/html/data/audit.log"}"
