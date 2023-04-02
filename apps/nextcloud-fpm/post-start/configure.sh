4f6a8b2389db6b7172f623d9a2d3c1c4241c79dc
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-02 13:08:34 +0300

    set chuch size

--- apps/nextcloud-fpm/post-start/configure.sh
+++ apps/nextcloud-fpm/post-start/configure.sh
@@ -9,10 +9,13 @@ occ config:system:set default_phone_region --value=${NEXT_DEFAULT_PHONE_REGION:-
 echo 'Setting "Shared" folder to "Shared"'
 occ config:system:set share_folder --value="Shared"
 
+echo 'Setting Max Chunk Size for Files'
+occ config:app:set files max_chunk_size --value="${NEXTCLOUD_CHUNKSIZE:-10485760}"
+
 echo 'Setting Overwrite URLs...'
-occ config:system:set overwrite.cli.url --value=${NEXT_OVERWRITE_CLI_URL:?"NEXT_OVERWRITE_CLI_URL is unset"}
-occ config:system:set overwritehost --value=${NEXT_OVERWRITE_HOST:?"NEXT_OVERWRITE_HOST is unset"}
-occ config:system:set overwriteprotocol --value=${NEXT_OVERWRITE_PROTOCOL:?"NEXT_OVERWRITE_PROTOCOL is unset"}
+occ config:system:set overwrite.cli.url --value="${NEXT_OVERWRITE_CLI_URL:?"NEXT_OVERWRITE_CLI_URL is unset"}"
+occ config:system:set overwritehost --value="${NEXT_OVERWRITE_HOST:?"NEXT_OVERWRITE_HOST is unset"}"
+occ config:system:set overwriteprotocol --value="${NEXT_OVERWRITE_PROTOCOL:?"NEXT_OVERWRITE_PROTOCOL is unset"}"
 
 echo 'Applying migrations/repairs/optimizations...'
 occ maintenance:repair
