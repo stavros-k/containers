51543e7499820eae924b2f838d15034dc130c710
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 22:51:08 +0300

    fix

--- apps/nextcloud-notify_push/scripts/entrypoint.sh
+++ apps/nextcloud-notify_push/scripts/entrypoint.sh
@@ -4,7 +4,7 @@
 
 HPB_HOST="${HPB_HOST:-kube.internal.healthcheck}"
 
-until curl -k -s -H "Host: $HPB_HOST" "$NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
+until curl -k -s -H "Host: $HPB_HOST" "$NEXTCLOUD_URL/status.php" | grep -q '"installed":true'; do
   echo 'Waiting Nextcloud to be installed and ready. Sleeping for 3s...'
   sleep 3
 done
