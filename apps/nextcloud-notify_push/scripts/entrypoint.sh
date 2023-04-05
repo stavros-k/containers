e89fe37070852a36c1ca16f0e66f03e0aa9d476e
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 22:45:14 +0300

    add header

--- apps/nextcloud-notify_push/scripts/entrypoint.sh
+++ apps/nextcloud-notify_push/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 [ -n "${NEXTCLOUD_URL:?"WARN: NEXTCLOUD_URL is unset"}" ]
 
-until curl -k -s "$NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
+until curl -k -s -H "Host: kube.internal.healthcheck" $NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
   echo 'Waiting Nextcloud to be installed and ready. Sleeping for 3s...'
   sleep 3
 done
