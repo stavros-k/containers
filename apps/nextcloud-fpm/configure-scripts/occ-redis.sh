891107f28eecadecf9a34ff87117d73eea94b72e
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 22:58:34 +0300

    change order

--- apps/nextcloud-fpm/configure-scripts/occ-redis.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-redis.sh
@@ -1,12 +1,12 @@
 occ_redis_install() {
   echo '## Configuring Redis...'
 
-  occ config:system:set memcache.local --value="\\OC\\Memcache\\APCu"
-  occ config:system:set memcache.distributed --value="\\OC\\Memcache\\Redis"
-  occ config:system:set memcache.locking --value="\\OC\\Memcache\\Redis"
   occ config:system:set redis host --value"${NX_REDIS_HOST:?"NX_REDIS_HOST is unset"}"
   occ config:system:set redis password --value"${NX_REDIS_PASS:?"NX_REDIS_PASS is unset"}"
   occ config:system:set redis port --value"${NX_REDIS_PORT:-6379}"
+  occ config:system:set memcache.local --value="\\OC\\Memcache\\APCu"
+  occ config:system:set memcache.distributed --value="\\OC\\Memcache\\Redis"
+  occ config:system:set memcache.locking --value="\\OC\\Memcache\\Redis"
 }
 
 occ_redis_remove() {
