768b48a394af910d35827e1e7da7100cf240dc31
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 21:53:43 +0300

    add entrypoint

---
+++ apps/nextcloud-notify_push/scripts/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+
+[ -n "${NEXTCLOUD_URL:?"WARN: NEXTCLOUD_URL is unset"}" ]
+
+until curl -k -s "$NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
+  echo 'Waiting Nextcloud to be installed and ready. Sleeping for 3s...'
+  sleep 3
+done
+
+if [ -n "${CONFIG_FILE:-}" ]; then
+  notify_push "$CONFIG_FILE"
+else
+  notify_push
+fi
