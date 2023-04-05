bc6926f4782960e4b8a93cb41d2fdadcf0f6cfd8
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 22:50:56 +0300

    fix

--- apps/nextcloud-notify_push/scripts/entrypoint.sh
+++ apps/nextcloud-notify_push/scripts/entrypoint.sh
@@ -2,7 +2,9 @@
 
 [ -n "${NEXTCLOUD_URL:?"WARN: NEXTCLOUD_URL is unset"}" ]
 
-until curl -k -s -H "Host: kube.internal.healthcheck" $NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
+HPB_HOST="${HPB_HOST:-kube.internal.healthcheck}"
+
+until curl -k -s -H "Host: $HPB_HOST" "$NEXTCLOUD_URL"/status.php | grep -q '"installed":true'; do
   echo 'Waiting Nextcloud to be installed and ready. Sleeping for 3s...'
   sleep 3
 done
