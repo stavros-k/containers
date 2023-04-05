35b9c4cebdc86832015b1bc36bd367d5dd9dd705
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-04-05 23:57:49 +0300

    Add some extra values for collabora

--- apps/nextcloud-fpm/configure-scripts/occ-collabora.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-collabora.sh
@@ -3,6 +3,8 @@ occ_collabora_install() {
   install_app richdocuments
 
   occ config:app:set richdocuments wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
+  occ config:app:set richdocuments public_wopi_url --value="${NX_COLLABORA_URL:?"NX_COLLABORA_URL is unset"}"
+  occ config:app:set richdocuments wopi_allowlist --value="${NX_COLLABORA_ALLOWLIST:?"NX_COLLABORA_ALLOWLIST is unset"}"
   occ config:system:set allow_local_remote_servers --value="true"
 }
 
@@ -11,5 +13,7 @@ occ_collabora_remove() {
   remove_app richdocuments
 
   occ config:app:delete richdocuments wopi_url
+  occ config:app:delete richdocuments public_wopi_url
+  occ config:app:delete richdocuments wopi_allowlist
   occ config:system:delete allow_local_remote_servers
 }
