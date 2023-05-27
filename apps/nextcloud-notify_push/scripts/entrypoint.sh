dba1193b966917bef8913cd3a84d7bc2b935dc0f
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-05-27 21:20:40 +0300

    Update entrypoint.sh

--- apps/nextcloud-notify_push/scripts/entrypoint.sh
+++ apps/nextcloud-notify_push/scripts/entrypoint.sh
@@ -9,6 +9,7 @@ until curl -m 5 -k -s -H "Host: $HPB_HOST" "$NEXTCLOUD_URL/status.php" | grep -q
   echo "Waiting Nextcloud [$NEXTCLOUD_URL] to be installed and ready. Sleeping for 3s..."
   sleep 3
 done
+echo "Nextcloud [$NEXTCLOUD_URL] replied, it is installed and ready. Starting Notify Push"
 
 if [ -n "${CONFIG_FILE:-}" ]; then
   notify_push "$CONFIG_FILE"
