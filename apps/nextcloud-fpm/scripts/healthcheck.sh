949b08d867fa5a6db54b11edaf84769ddcaff395
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-05-28 14:23:44 +0300

    Update healthcheck.sh

--- apps/nextcloud-fpm/scripts/healthcheck.sh
+++ apps/nextcloud-fpm/scripts/healthcheck.sh
@@ -3,4 +3,4 @@
 REQUEST_METHOD="GET" 
 SCRIPT_NAME="status.php" 
 SCRIPT_FILENAME="status.php" 
-timeout 3 cgi-fcgi -bind -connect "127.0.0.1:9000" | grep -q '"installed":true' || exit 1
+timeout 5 cgi-fcgi -bind -connect "127.0.0.1:9000" | grep -q '"installed":true' || exit 1
