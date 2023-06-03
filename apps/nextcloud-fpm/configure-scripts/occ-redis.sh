521e08d38280d6c31f438fb109cd3ce1b661525b
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2023-06-03 14:57:48 +0300

    Update occ-redis.sh

--- apps/nextcloud-fpm/configure-scripts/occ-redis.sh
+++ apps/nextcloud-fpm/configure-scripts/occ-redis.sh
@@ -2,6 +2,12 @@
 occ_redis_install() {
   echo '## Configuring Redis...'
 
+  # Nextcloud will try to connect to redis once you set even 1 
+  # parameter, so we first disable redis, set its credentials 
+  # and enable it again
+  occ config:system:delete memcache.distributed
+  occ config:system:delete memcache.locking
+  
   occ config:system:set redis host --value="${NX_REDIS_HOST:?"NX_REDIS_HOST is unset"}"
   occ config:system:set redis password --value="${NX_REDIS_PASS:?"NX_REDIS_PASS is unset"}"
   occ config:system:set redis port --value="${NX_REDIS_PORT:-6379}"
