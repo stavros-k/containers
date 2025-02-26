47b537fb06248a24b1136559152e10048df573cb
Stavros Kois <s.kois@outlook.com>
2025-02-27 00:03:22 +0200

    add logs and test

--- apps/nextcloud-fpm/scripts/run-cron-daemon.sh
+++ apps/nextcloud-fpm/scripts/run-cron-daemon.sh
@@ -9,4 +9,13 @@ done
 
 cron_file="${CRON_TAB_FILE:-/crontasks}"
 
+# Test crontab file
+echo "Testing crontab file ${cron_file}"
+echo "You might see some parsing errors here, but if the last line says crontab is valid, everything is ok."
+echo "The errors are because it tries to parse each crontab entry with both 5 and 6 fields."
+/usr/local/bin/supercronic -debug -test "${cron_file}" || {
+  echo "ERROR: crontab file is invalid"
+  exit 1
+}
+
 /usr/local/bin/supercronic "${cron_file}"
