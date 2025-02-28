117463bacda2f6c6d237dd13d6034021ff83f63d
Stavros Kois <s.kois@outlook.com>
2025-02-28 14:05:19 +0200

    improve add/remove apps

--- apps/nextcloud-fpm/configure-scripts/helpers.sh
+++ apps/nextcloud-fpm/configure-scripts/helpers.sh
@@ -6,11 +6,20 @@ install_app() {
 
   echo "Installing [$app_name]..."
 
-  if occ app:list | grep -wq "$app_name"; then
-    echo "App [$app_name] is already installed! Skipping..."
+  disabled_apps=$(occ app:list | yq '.Disabled')
+  if echo "$disabled_apps" | grep -wq "$app_name"; then
+    echo "App [$app_name] is installed but disabled! Enabling..."
+    occ app:enable "$app_name"
     return 0
   fi
 
+  enabled_apps=$(occ app:list | yq '.Enabled')
+  if echo "$enabled_apps" | grep -wq "$app_name"; then
+    echo "App [$app_name] is installed and enabled! Skipping..."
+    return 0
+  fi
+
+  # Here the app is not installed and we need to install it
   if ! occ app:install "$app_name"; then
     echo "Failed to install $app_name..."
     exit 1
@@ -24,11 +33,21 @@ remove_app() {
 
   echo "Removing [$app_name]..."
 
-  if ! occ app:list | grep -wq "$app_name"; then
+  disabled_apps=$(occ app:list | yq '.Disabled')
+  if echo "$disabled_apps" | grep -wq "$app_name"; then
+    echo "App [$app_name] is disabled! Enabling temporarily to remove..."
+    echo "Reason: https://github.com/nextcloud/server/issues/49689"
+    occ app:enable "$app_name"
+  fi
+
+  # Here the app is either enabled or not installed. Skip if not installed
+  enabled_apps=$(occ app:list | yq '.Enabled')
+  if ! echo "$enabled_apps" | grep -wq "$app_name"; then
     echo "App [$app_name] is not installed! Skipping..."
     return 0
   fi
 
+  # Here the app is installed and enabled. Remove it
   if ! occ app:remove "$app_name"; then
     echo "Failed to remove [$app_name]..."
     exit 1
@@ -57,35 +76,35 @@ set_list() {
     # Replace spaces with newlines so the input can have
     # mixed entries of space or new line separated values
     echo "$space_delimited_values" | tr ' ' '\n' | while IFS= read -r value; do
-        # Skip empty values
-        if [ -z "$value" ]; then
-          continue
-        fi
+      # Skip empty values
+      if [ -z "$value" ]; then
+        continue
+      fi
 
-        # Prepend prefix (eg OC\Preview)
-        if [ -n "${prefix}" ]; then
-          value="$prefix$value"
-        fi
+      # Prepend prefix (eg OC\Preview)
+      if [ -n "${prefix}" ]; then
+        value="$prefix$value"
+      fi
 
-        if [ "${app}" != 'system' ]; then
-          occ config:app:set "$app" "$list_name" $IDX --value="$value"
-        else
-          occ config:system:set "$list_name" $IDX --value="$value"
-        fi
+      if [ "${app}" != 'system' ]; then
+        occ config:app:set "$app" "$list_name" $IDX --value="$value"
+      else
+        occ config:system:set "$list_name" $IDX --value="$value"
+      fi
 
-        IDX=$((IDX+1))
+      IDX=$((IDX + 1))
     done
   fi
 }
 
 extract_domain() {
   url="$1"
-  
+
   # Remove http(s):// from URL
   domain="${url#*://}"
-  
+
   # Remove /foo (subfolder) from domain
   domain="${domain%%/*}"
-  
+
   echo "$domain"
 }
