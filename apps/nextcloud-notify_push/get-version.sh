65f840b9083ffa1707edfabc8ae35fc87bd914da
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-05-07 23:16:22 +0300

    fix

--- apps/nextcloud-notify_push/get-version.sh
+++ apps/nextcloud-notify_push/get-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curr_dir="$1"
 
-VERSION=$(grep "ENV NOTIFY_PUSH_VERSION" "$curr_dir"/Dockerfile | cut -d ' ' -f3)
+VERSION=$(grep "ENV NOTIFY_PUSH_VERSION" "$curr_dir"/Dockerfile | cut -d '=' -f2)
 
 echo "$VERSION"
